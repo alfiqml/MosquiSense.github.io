9712ca09c55f490fdcc8df7fca21013077d783c0
alfiqml <2020489786@isiswa.uitm.edu.my>
2023-07-10 01:50:51 +0800

    Update settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v3v5vfsn0xxjtmb=eoawoiw$5br4g0r&jy_l39995h_93l+-z5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mosquisense.onrender.com']
 
